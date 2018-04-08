e7db087b236be632b073c68325b097226c71fe0b
Sergei Butko <svbutko@hotmail.com>
2018-04-08 14:38:13 +0300

    Fix propTypes issue

--- src/index.js
+++ src/index.js
@@ -9,24 +9,26 @@
 'use strict';
 
 var React = require('react');
+var PropTypes = require('prop-types');
 var ReactNative = require('react-native');
+var createReactClass = require('create-react-class');
 var { Animated, Touchable } = ReactNative;
 
-var EdgeInsetsPropType = React.PropTypes.shape({
-  top: React.PropTypes.number,
-  left: React.PropTypes.number,
-  bottom: React.PropTypes.number,
-  right: React.PropTypes.number
+var EdgeInsetsPropType = PropTypes.shape({
+    top: PropTypes.number,
+    left: PropTypes.number,
+    bottom: PropTypes.number,
+    right: PropTypes.number
 });
 
 type Event = Object;
 
 type State = {
-  animationID: ?number;
-  scale: Animated.Value;
+    animationID: ?number;
+    scale: Animated.Value;
 };
 
-var PRESS_RETENTION_OFFSET = {top: 20, left: 20, right: 20, bottom: 30};
+var PRESS_RETENTION_OFFSET = { top: 20, left: 20, right: 20, bottom: 30 };
 
 /*
  * Example of using the `TouchableMixin` to play well with other responder
@@ -35,118 +37,118 @@ var PRESS_RETENTION_OFFSET = {top: 20, left: 20, right: 20, bottom: 30};
  * `TouchableMixin` expects us to implement some abstract methods to handle
  * interesting interactions such as `handleTouchablePress`.
  */
-var TouchableBounce = React.createClass({
-  mixins: [Touchable.Mixin],
+var TouchableBounce = createReactClass({
+    mixins: [Touchable.Mixin],
 
-  propTypes: {
-    onPress: React.PropTypes.func,
-    onPressIn: React.PropTypes.func,
-    onPressOut: React.PropTypes.func,
-    // The function passed takes a callback to start the animation which should
-    // be run after this onPress handler is done. You can use this (for example)
-    // to update UI before starting the animation.
-    onPressWithCompletion: React.PropTypes.func,
-    // the function passed is called after the animation is complete
-    onPressAnimationComplete: React.PropTypes.func,
+    propTypes: {
+        onPress: PropTypes.func,
+        onPressIn: PropTypes.func,
+        onPressOut: PropTypes.func,
+        // The function passed takes a callback to start the animation which should
+        // be run after this onPress handler is done. You can use this (for example)
+        // to update UI before starting the animation.
+        onPressWithCompletion: PropTypes.func,
+        // the function passed is called after the animation is complete
+        onPressAnimationComplete: PropTypes.func,
+        /**
+         * When the scroll view is disabled, this defines how far your touch may
+         * move off of the button, before deactivating the button. Once deactivated,
+         * try moving it back and you'll see that the button is once again
+         * reactivated! Move it back and forth several times while the scroll view
+         * is disabled. Ensure you pass in a constant to reduce memory allocations.
+         */
+        pressRetentionOffset: EdgeInsetsPropType,
+        /**
+         * This defines how far your touch can start away from the button. This is
+         * added to `pressRetentionOffset` when moving off of the button.
+         * ** NOTE **
+         * The touch area never extends past the parent view bounds and the Z-index
+         * of sibling views always takes precedence if a touch hits two overlapping
+         * views.
+         */
+        hitSlop: EdgeInsetsPropType,
+    },
+
+    getInitialState: function (): State {
+        return {
+            ...this.touchableGetInitialState(),
+            scale: new Animated.Value(1),
+        };
+    },
+
+    bounceTo: function (
+        value: number,
+        velocity: number,
+        bounciness: number,
+        callback?: ?Function
+    ) {
+        Animated.spring(this.state.scale, {
+            toValue: value,
+            velocity,
+            bounciness,
+        }).start(callback);
+    },
+
     /**
-     * When the scroll view is disabled, this defines how far your touch may
-     * move off of the button, before deactivating the button. Once deactivated,
-     * try moving it back and you'll see that the button is once again
-     * reactivated! Move it back and forth several times while the scroll view
-     * is disabled. Ensure you pass in a constant to reduce memory allocations.
+     * `Touchable.Mixin` self callbacks. The mixin will invoke these if they are
+     * defined on your component.
      */
-    pressRetentionOffset: EdgeInsetsPropType,
-    /**
-     * This defines how far your touch can start away from the button. This is
-     * added to `pressRetentionOffset` when moving off of the button.
-     * ** NOTE **
-     * The touch area never extends past the parent view bounds and the Z-index
-     * of sibling views always takes precedence if a touch hits two overlapping
-     * views.
-     */
-    hitSlop: EdgeInsetsPropType,
-  },
+    touchableHandleActivePressIn: function (e: Event) {
+        this.bounceTo(0.93, 0.1, 0);
+        this.props.onPressIn && this.props.onPressIn(e);
+    },
 
-  getInitialState: function(): State {
-    return {
-      ...this.touchableGetInitialState(),
-      scale: new Animated.Value(1),
-    };
-  },
+    touchableHandleActivePressOut: function (e: Event) {
+        this.bounceTo(1, 0.4, 0);
+        this.props.onPressOut && this.props.onPressOut(e);
+    },
 
-  bounceTo: function(
-    value: number,
-    velocity: number,
-    bounciness: number,
-    callback?: ?Function
-  ) {
-    Animated.spring(this.state.scale, {
-      toValue: value,
-      velocity,
-      bounciness,
-    }).start(callback);
-  },
+    touchableHandlePress: function (e: Event) {
+        var onPressWithCompletion = this.props.onPressWithCompletion;
+        if (onPressWithCompletion) {
+            onPressWithCompletion(() => {
+                this.state.scale.setValue(0.93);
+                this.bounceTo(1, 10, 10, this.props.onPressAnimationComplete);
+            });
+            return;
+        }
 
-  /**
-   * `Touchable.Mixin` self callbacks. The mixin will invoke these if they are
-   * defined on your component.
-   */
-  touchableHandleActivePressIn: function(e: Event) {
-    this.bounceTo(0.93, 0.1, 0);
-    this.props.onPressIn && this.props.onPressIn(e);
-  },
-
-  touchableHandleActivePressOut: function(e: Event) {
-    this.bounceTo(1, 0.4, 0);
-    this.props.onPressOut && this.props.onPressOut(e);
-  },
-
-  touchableHandlePress: function(e: Event) {
-    var onPressWithCompletion = this.props.onPressWithCompletion;
-    if (onPressWithCompletion) {
-      onPressWithCompletion(() => {
-        this.state.scale.setValue(0.93);
         this.bounceTo(1, 10, 10, this.props.onPressAnimationComplete);
-      });
-      return;
+        this.props.onPress && this.props.onPress(e);
+    },
+
+    touchableGetPressRectOffset: function (): typeof PRESS_RETENTION_OFFSET {
+        return this.props.pressRetentionOffset || PRESS_RETENTION_OFFSET;
+    },
+
+    touchableGetHitSlop: function (): ?Object {
+        return this.props.hitSlop;
+    },
+
+    touchableGetHighlightDelayMS: function (): number {
+        return 0;
+    },
+
+    render: function (): ReactElement {
+        return (
+            <Animated.View
+                style={[{ transform: [{ scale: this.state.scale }] }, this.props.style]}
+                accessible={true}
+                accessibilityLabel={this.props.accessibilityLabel}
+                accessibilityComponentType={this.props.accessibilityComponentType}
+                accessibilityTraits={this.props.accessibilityTraits}
+                testID={this.props.testID}
+                hitSlop={this.props.hitSlop}
+                onStartShouldSetResponder={this.touchableHandleStartShouldSetResponder}
+                onResponderTerminationRequest={this.touchableHandleResponderTerminationRequest}
+                onResponderGrant={this.touchableHandleResponderGrant}
+                onResponderMove={this.touchableHandleResponderMove}
+                onResponderRelease={this.touchableHandleResponderRelease}
+                onResponderTerminate={this.touchableHandleResponderTerminate}>
+                {this.props.children}
+            </Animated.View>
+        );
     }
-
-    this.bounceTo(1, 10, 10, this.props.onPressAnimationComplete);
-    this.props.onPress && this.props.onPress(e);
-  },
-
-  touchableGetPressRectOffset: function(): typeof PRESS_RETENTION_OFFSET {
-    return this.props.pressRetentionOffset || PRESS_RETENTION_OFFSET;
-  },
-
-  touchableGetHitSlop: function(): ?Object {
-    return this.props.hitSlop;
-  },
-
-  touchableGetHighlightDelayMS: function(): number {
-    return 0;
-  },
-
-  render: function(): ReactElement {
-    return (
-      <Animated.View
-        style={[{transform: [{scale: this.state.scale}]}, this.props.style]}
-        accessible={true}
-        accessibilityLabel={this.props.accessibilityLabel}
-        accessibilityComponentType={this.props.accessibilityComponentType}
-        accessibilityTraits={this.props.accessibilityTraits}
-        testID={this.props.testID}
-        hitSlop={this.props.hitSlop}
-        onStartShouldSetResponder={this.touchableHandleStartShouldSetResponder}
-        onResponderTerminationRequest={this.touchableHandleResponderTerminationRequest}
-        onResponderGrant={this.touchableHandleResponderGrant}
-        onResponderMove={this.touchableHandleResponderMove}
-        onResponderRelease={this.touchableHandleResponderRelease}
-        onResponderTerminate={this.touchableHandleResponderTerminate}>
-        {this.props.children}
-      </Animated.View>
-    );
-  }
 });
 
 module.exports = TouchableBounce;
